7d6f1ee19d08a4489ebadf6554db7723f5d988b3
Hju Kneyck Flores <hjucode@gmail.com>
2025-05-05 12:04:04 +0800

    feat: configure Vite server with host and origin settings

--- ticketing-app/vite.config.ts
+++ ticketing-app/vite.config.ts
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    origin: "http://monitor.cpro.net.au",
   },
 });
